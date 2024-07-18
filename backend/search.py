a3425c17a1209ee5896909741ddc34c169f20c2d
Seraph <rexchao27@Gmail.com>
2024-07-18 14:55:40 +0800

    fix: another small

--- backend/search.py
+++ backend/search.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 from collections import defaultdict
 import json
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 api_key = os.environ.get("API_KEY")
 
 client = OpenAI(
