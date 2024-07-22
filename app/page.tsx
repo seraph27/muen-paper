aeb81e4656e74bb4022668245ea3100e0582fb3c
Seraph <rexchao27@Gmail.com>
2024-07-22 13:16:25 +0800

    chore: bigger file

--- app/page.tsx
+++ app/page.tsx
@@ -61,6 +61,7 @@ export default function Home() {
                             key
                           )}`}
                           target="_blank"
+                          rel="noopener noreferrer" 
                         >
                           <ChevronRight className="icon" />
                         </Link>
